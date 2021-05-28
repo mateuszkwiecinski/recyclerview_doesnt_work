42e22fa5c3f9a41b993a6d44c2db975c3f5a33e8
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-05-28 18:24:29 +0200

    recyclerview doesn't work

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.coroutines.delay
-import kotlin.time.ExperimentalTime
-import kotlin.time.seconds
 
 internal class MainActivity : AppCompatActivity() {
 
@@ -25,41 +27,85 @@ internal class MainActivity : AppCompatActivity() {
         pager.adapter = object : FragmentStateAdapter(this) {
             override fun getItemCount() = 2
 
-            override fun createFragment(position: Int) = RefreshableFragment(position)
+            override fun createFragment(position: Int) =
+                if (position == 2) {
+                    RecyclerviewFragment()
+                } else {
+                    RefreshableFragment()
+                }
 
         }
         val tabs = findViewById<TabLayout>(R.id.tabs)
         TabLayoutMediator(tabs, pager) { tab, position ->
             tab.text = "Tab $position"
         }.attach()
+        findViewById<RecyclerView>(R.id.recycler).setup(this)
     }
 }
 
-class RefreshableFragment(private val position: Int) : Fragment(R.layout.fragment_refreshable) {
+class RefreshableFragment : Fragment(R.layout.fragment_with_switch)
 
-    lateinit var swipeRefresh: SwipeRefreshLayout
-    lateinit var label: TextView
+class RecyclerviewFragment : Fragment(R.layout.fragment_recyclerview) {
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        swipeRefresh = view.findViewById(R.id.swipeRefresh)
-        label = view.findViewById(R.id.label)
+        view.findViewById<RecyclerView>(R.id.recycler).setup(requireContext())
 
-        label.text = "Initial $position"
-        swipeRefresh.setOnRefreshListener { action() }
+    }
+}
+
+fun RecyclerView.setup(context: Context) {
+    layoutManager = LinearLayoutManager(context)
+    adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ) = object : RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(viewType, parent, false)
+        ) {
+
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return if (position % 20 == 0) {
+                R.layout.item_with_switch
+            } else {
+                R.layout.item_with_text
+            }
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val label = when (holder.itemViewType) {
+                R.layout.item_with_switch -> holder.itemView.findViewById<SwitchMaterial>(R.id.item_switch)
+                R.layout.item_with_text -> holder.itemView.findViewById<TextView>(R.id.item_text)
+                else -> error("unsupported")
+            }
+
+            label.text = "Item $position"
+        }
+
+        override fun getItemCount(): Int = 100
+
+    }
+}
+
+class SwiMatChTerial(context: Context, attrs: AttributeSet?) : SwitchMaterial(context, attrs) {
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
     }
 
-    @OptIn(ExperimentalTime::class)
-    fun action() {
-        lifecycleScope.launchWhenCreated {
-            swipeRefresh.isRefreshing = true
-            var left = 10.seconds
-            while (left > 0.seconds) {
-                left -= 1.seconds
-                label.text = "Loading ${left}"
-                delay(1.seconds)
-            }
-            label.text = "Loaded"
-            swipeRefresh.isRefreshing = false
-        }
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+    }
+
+    override fun setChecked(checked: Boolean) {
+        super.setChecked(checked)
+    }
+
+    override fun requestLayout() {
+        super.requestLayout()
     }
 }
